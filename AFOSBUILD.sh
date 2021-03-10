543baad06a36e143e66abbf925ab3dc528e08ed7
TCT Source Code TEAM <source@thecrackertechnology.com>
2021-03-10 16:04:01 -0300

    Update AFOSBUILD.sh again

--- AFOSBUILD.sh
+++ AFOSBUILD.sh
@@ -4,8 +4,14 @@ cp -Rf andraxbin/* /opt/ANDRAX/bin
 
 chmod -R 755 /opt/ANDRAX/bin
 
+WORKDIR=$(pwd)
+
+cp -Rf $(pwd) /opt/ANDRAX/mitmproxy
+
+cd /opt/ANDRAX/mitmproxy
+
 /opt/ANDRAX/python38/bin/python3 -m venv venv
 
 venv/bin/pip install -e ".[dev]"
 
-cp -Rf $(pwd) /opt/ANDRAX/mitmproxy
+cd $WORKDIR
